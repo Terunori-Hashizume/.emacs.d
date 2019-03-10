c1ac95aaa4602853cf4e92424922f366e7108c89
Terunori-Hashizume <hashizume.terunori@gmail.com>
2019-03-10 16:17:13 +0900

    correct atcoder lisp

--- mylisp/atcoder.el
+++ mylisp/atcoder.el
@@ -1,6 +1,6 @@
 (defun run_test_cases ()
   (interactive)
   (setq-local run "~/kyo_pro/tools/run_test_cases.sh")
-  (shell-command-to-string (concat (run " " (buffer-name)))))  
+  (shell-command (concat run " " (expand-file-name (buffer-name)))))  
 
 (global-set-key (kbd "C-x t c") 'run_test_cases)
